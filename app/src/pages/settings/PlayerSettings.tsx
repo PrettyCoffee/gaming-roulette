4997cc411d5f4d897817912bc6c62f04eb57cad3
PrettyCoffee <jonathan-holz@outlook.com>
2024-01-23 22:07:21 +0100

    refactor(PlayerSettings): Extract EditPlayer component

--- app/src/pages/settings/PlayerSettings.tsx
+++ app/src/pages/settings/PlayerSettings.tsx
@@ -1,33 +1,52 @@
 // eslint-disable-next-line @pretty-cozy/no-unspecific-imports
+
+import { Trash } from "lucide-react"
+
 import { ColorPicker } from "~/components/ColorPicker"
+import { IconButton } from "~/components/IconButton"
 import { InputLabel } from "~/components/InputLabel"
 import { Input } from "~/components/ui/input"
-import { usePlayers } from "~/data/players"
+import { Player, usePlayers } from "~/data/players"
 import { colors } from "~/utils/colors"
 
+const EditPlayer = ({ id, name, color, index }: Player & { index: number }) => {
+  const { setPlayerAttribute, removePlayer } = usePlayers()
+  return (
+    <>
+      <InputLabel className="mb-2" htmlFor={id}>
+        {`Player #${index + 1}`}
+      </InputLabel>
+      <div className="flex gap-2">
+        <Input
+          id={id}
+          value={name}
+          onChange={({ target }) =>
+            setPlayerAttribute(id, "name", target.value)
+          }
+        />
+        <ColorPicker
+          value={color}
+          onChange={color => setPlayerAttribute(id, "color", color)}
+          colors={colors}
+        />
+        <IconButton
+          variant="outline"
+          onClick={() => removePlayer(id)}
+          icon={Trash}
+          title="Remove player"
+        />
+      </div>
+    </>
+  )
+}
+
 export const PlayerSettings = () => {
-  const { players, setPlayerAttribute } = usePlayers()
+  const { players } = usePlayers()
   return (
     <div className="grid grid-cols-2 gap-2 p-2 pt-0">
-      {players.map(({ id, name, color }, index) => (
-        <div key={id} className="col-span-1">
-          <InputLabel className="mb-2" htmlFor={id}>
-            {`Player #${index + 1}`}
-          </InputLabel>
-          <div className="flex gap-2">
-            <Input
-              id={id}
-              value={name}
-              onChange={({ target }) =>
-                setPlayerAttribute(id, "name", target.value)
-              }
-            />
-            <ColorPicker
-              value={color}
-              onChange={color => setPlayerAttribute(id, "color", color)}
-              colors={colors}
-            />
-          </div>
+      {players.map((player, index) => (
+        <div key={player.id} className="col-span-1">
+          <EditPlayer {...player} index={index} />
         </div>
       ))}
     </div>
