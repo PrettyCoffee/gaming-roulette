9351b17ea980ff5217bbc285f3deed61bae304c7
PrettyCoffee <jonathan-holz@outlook.com>
2024-03-08 01:30:32 +0100

    feat(textarea): Add lines prop

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,19 +1,89 @@
 import * as React from "react"
 
+import { getFontSize } from "~/utils/getFontSize"
 import { cn } from "~/utils/utils"
 
-export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
+const getLineHeight = (element: HTMLElement): number => {
+  const rem = getFontSize()
+  const fontSize = getFontSize(element)
+  const lineHeight = getComputedStyle(element).lineHeight
+
+  const value = lineHeight.match(/(\d+\.?\d*)/)?.[1]
+  const unit = lineHeight.replace(value ?? "", "")
+
+  const fallback = fontSize * 1.3 // some random fallback with no meaning
+  if (value == null) return fallback
+
+  switch (unit) {
+    case "px":
+      return parseFloat(value)
+    case "rem":
+      return parseFloat(value) * rem
+    case "em":
+    case "":
+      return parseFloat(value) * fontSize
+    default:
+      return fallback
+  }
+}
+
+const getYPadding = (element: HTMLElement) => {
+  const { paddingTop, paddingBottom } = getComputedStyle(element)
+  return parseFloat(paddingTop) + parseFloat(paddingBottom)
+}
+
+const getYBorder = (element: HTMLElement) => {
+  const { borderTopWidth, borderBottomWidth } = getComputedStyle(element)
+  return parseFloat(borderTopWidth) + parseFloat(borderBottomWidth)
+}
+
+const combineRefs =
+  <T,>(...refs: React.Ref<T>[]): React.LegacyRef<T> =>
+  (element: T) => {
+    refs.forEach(ref => {
+      if (typeof ref === "function") {
+        ref(element)
+      } else if (ref != null) {
+        ;(ref as React.MutableRefObject<T | null>).current = element
+      }
+    })
+  }
+
+interface ClampedHeightProps {
+  lines?: number
+}
+const useClampedHeight = ({ lines }: ClampedHeightProps) => {
+  const [ref, setRef] = React.useState<HTMLTextAreaElement | null>(null)
+
+  const height = React.useMemo(() => {
+    const element = ref
+    if (element == null || !lines) return undefined
+    const lineHeight = getLineHeight(element)
+    const padding = getYPadding(element)
+    const border = getYBorder(element)
+    const height = lines * lineHeight + padding + border
+    return `${height}px`
+  }, [lines, ref])
+
+  return { ref: (element: HTMLTextAreaElement) => setRef(element), height }
+}
+
+export type TextareaProps =
+  React.TextareaHTMLAttributes<HTMLTextAreaElement> & { lines?: number }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, lines, ...props }, forwardedRef) => {
+    const { ref, height } = useClampedHeight({ lines })
+
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[80px] w-full resize-none rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        ref={ref}
+        ref={combineRefs(ref, forwardedRef)}
         {...props}
+        style={{ height, ...props.style }}
       />
     )
   }
--- src/pages/current/Current.tsx
+++ src/pages/current/Current.tsx
@@ -8,7 +8,6 @@ import { Textarea } from "~/components/ui/textarea"
 import { Tooltip } from "~/components/ui/tooltip"
 import { Player, usePlayerGameStats, usePlayers } from "~/data/players"
 import { useRuleset } from "~/data/ruleset"
-import { getFontSize } from "~/utils/getFontSize"
 import { cn } from "~/utils/utils"
 
 const Counter = ({ current, limit }: { limit: number; current: number }) => (
@@ -26,52 +25,19 @@ interface PlayerGamesProps extends Player {
   onChange: Dispatch<string[]>
 }
 
-const getLineHeight = (element: HTMLElement): number => {
-  const rem = getFontSize()
-  const fontSize = getFontSize(element)
-  const lineHeight = getComputedStyle(element).lineHeight
-
-  const value = lineHeight.match(/(\d+\.?\d*)/)?.[1]
-  const unit = lineHeight.replace(value ?? "", "")
-
-  const fallback = fontSize * 1.3 // some random fallback with no meaning
-  if (value == null) return fallback
-
-  switch (unit) {
-    case "px":
-      return parseFloat(value)
-    case "rem":
-      return parseFloat(value) * rem
-    case "em":
-    case "":
-      return parseFloat(value) * fontSize
-    default:
-      return fallback
-  }
-}
-
-const getYPadding = (element: HTMLElement) => {
-  const { paddingTop, paddingBottom } = getComputedStyle(element)
-  return parseFloat(paddingTop) + parseFloat(paddingBottom)
-}
-
-const getYBorder = (element: HTMLElement) => {
-  const { borderTopWidth, borderBottomWidth } = getComputedStyle(element)
-  return parseFloat(borderTopWidth) + parseFloat(borderBottomWidth)
-}
-
 const ErrorHint = ({ errors }: { errors: string[] }) => (
   <Tooltip.Root>
     <Tooltip.Trigger asChild>
-      <div>
+      <span className="inline-flex">
         <Icon icon={AlertTriangle} color="error" size="sm" />
-      </div>
+      </span>
     </Tooltip.Trigger>
     <Tooltip.Content
       className="border-red-500"
       side="bottom"
       align="end"
       alignOffset={-8}
+      sideOffset={8}
     >
       <ul>
         {errors.map(error => (
@@ -89,7 +55,6 @@ const PlayerGames = ({
   name,
   onChange,
 }: PlayerGamesProps) => {
-  const [ref, setRef] = useState<HTMLTextAreaElement | null>(null)
   const [rules] = useRuleset()
   const stats = usePlayerGameStats()[id]
   const [value, setValue] = useState(games.join("\n"))
@@ -115,16 +80,6 @@ const PlayerGames = ({
     onChange(games.split("\n").filter(Boolean))
   }
 
-  const height = useMemo(() => {
-    if (ref == null) return "0px"
-    const lineHeight = getLineHeight(ref)
-    const padding = getYPadding(ref)
-    const border = getYBorder(ref)
-    const height =
-      Math.min(rules.gamesPerPerson, 10) * lineHeight + padding + border
-    return `${height}px`
-  }, [rules.gamesPerPerson, ref])
-
   return (
     <div key={id} className="col-span-1">
       <div className="flex px-3 items-center gap-2">
@@ -134,11 +89,10 @@ const PlayerGames = ({
         {errors.length > 0 && <ErrorHint errors={errors} />}
       </div>
       <Textarea
-        ref={setRef}
         id={id}
         value={value}
         onChange={({ target }) => setGames(target.value)}
-        style={{ height }}
+        lines={Math.min(rules.gamesPerPerson, 10)}
         className={cn(
           "whitespace-pre resize-none",
           `border-${color}-200`,
