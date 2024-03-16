c205bc95fa45cb7481d2abdf6b6102aaf61143af
PrettyCoffee <jonathan-holz@outlook.com>
2024-03-16 23:57:59 +0100

    feat(NoData): Add component

---
+++ src/components/NoData.tsx
@@ -0,0 +1,67 @@
+import { keyframes, css } from "goober"
+import { Ghost } from "lucide-react"
+
+import { cn } from "~/utils/utils"
+
+import { Icon } from "./Icon"
+
+const rotate = keyframes`
+  0%, 100% {
+    translate: 1rem;
+    rotate: -10deg;
+    top: 0;
+  }
+  25%, 75% {
+    top: -1rem;
+  }
+  50% {
+    translate: -1rem;
+    rotate: 10deg;
+    top: 0;
+  }
+`
+
+const shadowSlide = keyframes`
+  0%, 100% {
+    transform: translateX(1.5rem);
+    width: 3.25rem;
+    height: 0.5rem;
+    opacity: 0.5;
+  }
+  25%, 75% {
+    width: 3.1rem;
+    opacity: 0.4;
+  }
+  50% {
+    transform: translateX(-1.5rem);
+    width: 3.25rem;
+    height: 0.5rem;
+    opacity: 0.5;
+  }
+`
+
+const shadow = css`
+  animation: ${shadowSlide} 2.5s infinite ease-in-out;
+`
+
+const animate = css`
+  animation: ${rotate} 2.5s infinite ease-in-out;
+`
+
+export const NoData = ({ label }: { label: string | JSX.Element }) => (
+  <div className="flex flex-col items-center gap-8">
+    <div className="relative flex size-20 items-center justify-center">
+      <Icon
+        icon={Ghost}
+        color="muted"
+        className={cn("absolute size-[4.5rem]", animate)}
+      />
+      <div
+        className={cn("absolute -bottom-4 rounded-[50%] bg-black", shadow)}
+      />
+    </div>
+    <span className="text-md block max-w-80 text-center font-bold text-muted-foreground">
+      {label}
+    </span>
+  </div>
+)
