35b98c1f43ef28d6e1434e7ebe0dee601f42eb84
PrettyCoffee <jonathan-holz@outlook.com>
2024-03-23 22:30:01 +0100

    fix(date): Add number pad to today fn

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,7 +5,10 @@ export const YEAR = DAY * 365
 
 export const today = () => {
   const date = new Date()
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+  const year = date.getFullYear().toString().padStart(4, "0")
+  const month = (date.getMonth() + 1).toString().padStart(2, "0")
+  const day = date.getDate().toString().padStart(2, "0")
+  return `${year}-${month}-${day}`
 }
 
 export const timeBetween = (date1 = today(), date2 = today()) =>
