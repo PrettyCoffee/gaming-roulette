6a3571ca5726b853e6c8e8dc80039a5895af0eed
PrettyCoffee <jonathan-holz@outlook.com>
2024-04-02 22:19:26 +0200

    refactor(SyncGithub): Clean up loading state

--- src/pages/settings/SyncGithub.tsx
+++ src/pages/settings/SyncGithub.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useState } from "react"
 
 import { RefreshCw } from "lucide-react"
 
@@ -7,47 +7,18 @@ import { LoadingData } from "~/components/LoadingData"
 import { Button } from "~/components/ui/button"
 import { useGames } from "~/data/gamesExternal"
 
-const useDelayValueChange = <T,>(value: T, delay = 500) => {
-  const [blockedValue, setBlockedValue] = useState(value)
-  const isBlocked = useRef(false)
-  const latestValue = useRef(value)
-  const timeout = useRef<NodeJS.Timeout>()
-
-  const blockChanges = useCallback(() => {
-    isBlocked.current = true
-    if (timeout.current) {
-      clearTimeout(timeout.current)
-    }
-    timeout.current = setTimeout(() => {
-      setBlockedValue(latestValue.current)
-      isBlocked.current = false
-    }, delay)
-  }, [delay])
-
-  useEffect(() => {
-    if (value == null) {
-      setBlockedValue(value)
-      blockChanges()
-      return
-    }
-
-    if (isBlocked.current) {
-      latestValue.current = value
-      return
-    }
-
-    setBlockedValue(value)
-  }, [value, blockChanges])
-
-  return blockedValue
-}
-
 export const SyncGithub = () => {
+  const [loading, setLoading] = useState(false)
   const { games, refreshGames } = useGames()
 
-  const debouncedGames = useDelayValueChange(games, 2000)
+  const onRefresh = () => {
+    refreshGames()
+    // Show loading state for at least 2 seconds
+    setLoading(true)
+    setTimeout(() => setLoading(false), 2000)
+  }
 
-  if (!debouncedGames)
+  if (loading || !games)
     return (
       <div className="fixed inset-0 z-50 flex size-full items-center justify-center bg-base/50">
         <LoadingData label="Loading data..." />
@@ -55,7 +26,7 @@ export const SyncGithub = () => {
     )
 
   return (
-    <Button variant="flat" onClick={refreshGames}>
+    <Button variant="flat" onClick={onRefresh}>
       <Icon icon={RefreshCw} size="sm" />
       Sync data
     </Button>
