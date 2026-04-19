b37f7770f7f2a53305a365e71c9d10c22cce4969
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-04-19 14:38:28 +0200

    chore: Update minor dependencies

--- src/app/layout/Navigation.tsx
+++ src/app/layout/Navigation.tsx
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority"
-import { Github } from "lucide-react"
+import { createLucideIcon } from "lucide-react"
 
 import { routes } from "app/routes/Router"
 import { BaseButton } from "components/buttons/BaseButton"
@@ -13,6 +13,17 @@ import { ClassNameProp } from "types/BaseProps"
 import { RouteMeta } from "types/routes"
 import { focusRing, noOverflow } from "utils/styles"
 import { cn } from "utils/utils"
+
+const GithubIcon = createLucideIcon("Github", [
+  [
+    "path",
+    {
+      d: "M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4",
+      key: "1",
+    },
+  ],
+  ["path", { d: "M9 18c-4.51 2-5-2-7-2", key: "2" }],
+])
 
 const navButton = cva(
   cn(
@@ -95,7 +106,7 @@ export const Navigation = ({ className }: ClassNameProp) => {
         )}
       >
         {/* eslint-disable-next-line @typescript-eslint/no-deprecated */}
-        <Icon icon={Github} size="sm" />
+        <Icon icon={GithubIcon} size="sm" />
         {!compactNavigation && "Github"}
       </Link>
     </nav>
